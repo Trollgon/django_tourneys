e333e181a04e9ad1a1719823a0029b7d65309f5c
Trollgon <studentsparky@web.de>
2020-04-07 16:05:13 +0200

    added navbar with loginbutton

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">Home</a>
+      <form class="form-inline">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+          <font-awesome-icon icon="sign-in-alt" /> Login
+        </button>
+      </form>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 import App from './App'
 import router from './router'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faSignInAlt} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.config.productionTip = false
+// Import the styles directly.
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+// use bootstrap
+Vue.use(BootstrapVue);
+
+// enable these fontawesome icons in all components
+library.add(faSignInAlt);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +28,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
